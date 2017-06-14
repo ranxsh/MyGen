ee45705caaeb988dc8ca4bc0cb4c4070613ea129
冉祥生 <ranxiangsheng@syswin.com>
2017-06-14 17:36:12 +0800

    新建项目

---
+++ src/main/java/com/xsr/demo/my_jeesite/App.java
@@ -0,0 +1,13 @@
+package com.xsr.demo.my_jeesite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
