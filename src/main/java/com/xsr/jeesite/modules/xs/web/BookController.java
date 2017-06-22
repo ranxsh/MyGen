91bfd2bb39f3cf9f1c938ad00b0681cd3335bfe6
ranxsh <ranxsh@github.com>
2017-06-22 23:46:00 +0800

    sys

---
+++ src/main/java/com/xsr/jeesite/modules/xs/web/BookController.java
@@ -0,0 +1,48 @@
+package com.xsr.jeesite.modules.xs.web;
+
+import com.xsr.jeesite.modules.xs.entity.Book;
+import com.xsr.jeesite.modules.xs.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
+public class BookController {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private BookService bookService;
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	private String list(Model model) {
+		List<Book> list = bookService.getList();
+		model.addAttribute("list", list);
+		// list.jsp + model = ModelAndView
+		return "list";// WEB-INF/jsp/"list".jsp
+	}
+
+	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+	private String detail(@PathVariable("bookId") Long bookId, Model model) {
+		if (bookId == null) {
+			return "redirect:/book/list";
+		}
+		Book book = bookService.getById(bookId);
+		if (book == null) {
+			return "forward:/book/list";
+		}
+		model.addAttribute("book", book);
+		return "detail";
+	}
+
+
+}
